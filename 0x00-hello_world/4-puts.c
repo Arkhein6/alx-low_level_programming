92cfa0c6e5593295e31a355e7c22bfbe90b4fde3
RedStar6 <richmondnkrumah04@gmail.com>
2023-03-16 19:01:18 -0700

    Low Level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
-int main()
-{
-    char Mystr[] = ""Programming is like building a multilingual puzzle";
-    
-    puts(Mystr);
+int main(void)
+{   
+    puts("Programming is like building a multilingual puzzle");
     return 0;
 }
 
