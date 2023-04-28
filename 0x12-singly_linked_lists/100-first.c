9943e5154cc488c6f1a6bd82ba2d484e242865a6
RedStar6 <richmondnkrumah04@gmail.com>
2023-04-28 03:37:36 +0000

    Linked Lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,8 +4,6 @@
  * startFunc - prints a string automatically before the main function using the constructor attribute
  *
  */
-void startFunc(void)  __attribute__ ((constructor))
-{
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+void __attribute__((constructor)) startFunc(void){
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n")
 }
